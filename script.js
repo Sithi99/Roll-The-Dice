8af08de8da2efccdedb953298bcbeafcbfb01783
Sithija Wickramasinghe <2020cs212@stu.ucsc.cmb.ac.lk>
2022-07-29 14:36:00 +0530

    New Edit

--- script.js
+++ script.js
@@ -6,31 +6,84 @@ let images = ["dice_1.png",
     "dice_6.png"]
     let dice = document.querySelectorAll("img");
 
+var Player1 ={
+    Name: "Player1",
+    Total: 0
+}
+
+var Player2 ={
+    Name: "Player2",
+    Total: 0
+}
+
+
+let scorePlayer1 = document.querySelector("#player1Score");
+let scorePlayer2 = document.querySelector("#player2Score");
+
+var currentPlayer = Player1;
 
 
 function rollDice()
 {
-    /*dice.forEach(function(die){
-        die.classList.add("shake")
-    });
-    setTimeout(function(){
-        dice.forEach(function(die){
-            die.classList.remove("shake");
-        });*/
-
+    
     let firstDiceValue = Math.floor(Math.random()*6 + 1);
-    let SecondDiceValue = Math.floor(Math.random()*6 + 1);
+    let secondDiceValue = Math.floor(Math.random()*6 + 1);
 
-    //console.log(firstDiceValue,SecondDiceValue);
+    console.log(firstDiceValue,secondDiceValue);
 
     document.querySelector("#dice-1").setAttribute("src",images[firstDiceValue - 1]);
-    document.querySelector("#dice-2").setAttribute("src",images[SecondDiceValue - 1]);
+    document.querySelector("#dice-2").setAttribute("src",images[secondDiceValue - 1]);
 
-    let total = firstDiceValue+SecondDiceValue;
+
+    let total = firstDiceValue + secondDiceValue;
     console.log(total);
 
+    if(firstDiceValue==1 && secondDiceValue==1)
+    {
+        
+        currentPlayer.Total = 0;
+        nextPlayer();
+        checkWinner();
+    }
+    else if(firstDiceValue == secondDiceValue)
+    {
+        currentPlayer.Total = currentPlayer.Total + total;
+        checkWinner();
+    }
+    else
+    {
+        currentPlayer.Total = currentPlayer.Total + total;
+        nextPlayer();
+        checkWinner();
+        
+    }
 
-    /*},
-    1000
-    );*/
+}
+
+function nextPlayer(){
+    if(currentPlayer.Name == "Player1")
+    {
+        scorePlayer1.innerHTML = "Score: "+currentPlayer.Total;
+        currentPlayer = Player2;
+        
+    }
+    else
+    {
+        scorePlayer2.innerHTML = "Score: "+currentPlayer.Total;
+        currentPlayer = Player1;
+        
+    }
+}
+
+function checkWinner()
+{
+    if(Player1.Total >= 100)
+    {
+        alert("Player 1 Wins");
+    }
+
+    if(Player2.Total >= 100)
+    {
+        alert("Player 2 Wins");
+    }
 }
